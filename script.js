9b8186efeb71b1fccd6d97f670ba73b26c10f501
frgwork <frgwork@ya.ru>
2022-02-08 16:14:35 +0300

    Chess board

---
+++ script.js
@@ -0,0 +1,28 @@
+function drawBoard() {
+  let board = document.querySelector('.board');
+  let block;
+  let flag = true; // белая-черная клетка
+
+  for (let i = 0; i < 8; i++) {
+
+    for (let j = 0; j < 8; j++) {
+
+      if (j == 0) flag = !flag;
+
+      block = document.createElement('div');
+
+      if (flag) {
+        block.classList = 'block black';
+      } else {
+        block.classList = 'block white';
+      }
+      //вывел
+      board.appendChild(block);
+
+      //поменял с черного на белое
+      flag = !flag;
+    }
+  }
+}
+
+drawBoard();
